92141ab4597fd989ad98a13e6e1a1be0009a3a54
eulub <eulub@KLAERIIP>
2023-08-21 15:26:18 +0300

    Add project files.

---
+++ GameProject1/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace GameProject1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GamerManager gamerManager = new GamerManager(new NewEStateUserValidationManager());
+            gamerManager.Add(new Gamer
+            {
+                Id = 1,
+                BirtYear = 2002,
+                FirstName = "Efekan",
+                LastName = "ULUBAŞ",
+                IdentityNumber = 54382823
+            });
+        }
+    }
+}
---
+++ GameProject1/UserValidationManager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameProject1
+{
+    internal class UserValidationManager : IUserValidationService
+    {
+        public bool Validate(Gamer gamer)
+        {
+            if    (gamer.BirtYear == 2002 
+                && gamer.FirstName == "Efekan" 
+                && gamer.LastName =="ULUBAŞ" 
+                && gamer.IdentityNumber == 54382823)
+            {
+                return true;
+            }
+             else 
+            {
+                return false; 
+            }
+
+        }
+    }
+}
